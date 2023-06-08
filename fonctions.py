504900d64fcbb7a6f533039c07efcb55876437ec
Tarek-Saci <tarek7saci@gmail.com>
2023-06-07 20:34:06 -0400

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,6 +1,11 @@
-def get_windy_data()
+import requests
 
-    url_request = url_base + str(latitude) + "/" + str(longitude)
+def get_windy_data(lat , lon):
+    url_base = "https://node.windy.com/forecast/meteogram/ecmwf/"
+    url_request = url_base + str(lat) + "/" + str(lon)
     import urllib.request, json
     with urllib.request.urlopen(url_request) as url:
         data = json.load(url)
+    print(data)
+
+get_windy_data(45.00 ,45.00)
