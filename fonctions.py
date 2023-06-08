0ccc373109a7c9d05b187ef61b9b15b822eeeadf
Tarek-Saci <tarek7saci@gmail.com>
2023-06-07 23:33:25 -0400

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,5 +1,6 @@
 import requests
-
+from deck import *
+import math
 
 '''
 def get_windy_data(lat , lon):
@@ -14,6 +15,30 @@ get_windy_data(45.00 ,45.00)
 '''
 
 
-def range(lat, lon, finesse, altitude, hauteur_aerodrome):
-    range = (altitude - hauteur_aerodrome) * finesse
+# -----peut etre definir une class pour le calcul GPS---------
+
+def range(finesse, altitude, hauteur_aerodrome):  # calcul du range
+    range = (altitude - hauteur_aerodrome) * finesse # on ne sait pas encore si on aura les altitudes des aerodromes
     return range
+
+
+def distance(lat1, lon1, lat2, lon2):
+    # Convertir les coordonnées degrés en radians
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    # Rayon de la Terre en mètres
+    rayon_terre = 6371 * 1000  # En mètres
+
+    # Calcul des différences de latitude et de longitude
+    d_lat = lat2 - lat1
+    d_lon = lon2 - lon1
+
+    # Calcul de la distance
+    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = rayon_terre * c
+
+    return distance
