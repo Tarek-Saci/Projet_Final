20662b68671ff9e619bec8576cecd9174e39b42e
Tarek-Saci <tarek7saci@gmail.com>
2023-06-09 13:48:27 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,7 +1,12 @@
 # --------------------------------------valeurs de test input------------------------------------------
+
 lat = 45.805
 lon = 47.555
-vitesse_avion = 200
+vitesse_avion = 200  # homogène au reste
+
 # --------------------------------------valeurs de test windy------------------------------------------
-vitesse_vent = 20
-direction_vent = 57
+
+vitesse_vent = 20  # homogène au reste
+direction_vent = 57  # Direction du vent en degrés
+
+# -----------------------------------------------------------------------------------------------------
