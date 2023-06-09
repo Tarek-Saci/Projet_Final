460ac9b6e2a45970c2acf012346f3b5967effb74
Tarek-Saci <tarek7saci@gmail.com>
2023-06-09 13:32:17 -0400

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,6 +1,6 @@
 import requests
-from deck import *
 import math
+from deck import *
 
 '''
 def get_windy_data(lat , lon):
@@ -17,12 +17,20 @@ get_windy_data(45.00 ,45.00)
 
 # -----peut etre definir une class pour le calcul GPS---------
 
-def range(finesse, altitude, hauteur_aerodrome):  # calcul du range
-    range = (altitude - hauteur_aerodrome) * finesse # on ne sait pas encore si on aura les altitudes des aerodromes
-    return range
+def range_plane(finesse, altitude, hauteur_aerodrome):  # calcul du range
+    range_theorique_plane = (altitude - hauteur_aerodrome) * finesse # on ne sait pas encore si on aura les altitudes des aerodromes
+    return range_theorique_plane
+
+def corriger_distance_franchissable(range_theorique, vitesse_avion, vitesse_vent, direction_vent):
+    vent_x = vitesse_vent * math.cos(math.radians(direction_vent))
+    vent_y = vitesse_vent * math.sin(math.radians(direction_vent))
+
+    vitesse_relative = math.sqrt((vitesse_avion - vent_x)**2 + vent_y**2)
+    range_corrige = (range_theorique / vitesse_avion) * vitesse_relative
+    return range_corrige
 
 
-def distance(lat1, lon1, lat2, lon2):
+def distance_entre_2_points(lat1, lon1, lat2, lon2):
     # Convertir les coordonnées degrés en radians
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
@@ -33,12 +41,13 @@ def distance(lat1, lon1, lat2, lon2):
     rayon_terre = 6371 * 1000  # En mètres
 
     # Calcul des différences de latitude et de longitude
-    d_lat = lat2 - lat1
-    d_lon = lon2 - lon1
+    diff_lat = lat2 - lat1
+    diff_lon = lon2 - lon1
 
     # Calcul de la distance
-    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    a = math.sin(diff_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(diff_lon / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     distance = rayon_terre * c
 
     return distance
+
