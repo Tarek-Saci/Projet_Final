77c132a784d7b813983375f946d1d818bb2a3155
SimGbd <133153776+SimGbd@users.noreply.github.com>
2023-06-13 17:16:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Importation des fichiers
-import Donnees as data
+import Donnees
 from LectureYAML import LecteurYAML
 from fonction_range import Performance
 
@@ -8,27 +8,28 @@ from fonction_range import Performance
 # Création de l'objet YAML qui lit le fichier "deck.yamL"
 parser = LecteurYAML('deck.yaml')
 # Lecture du fichier avec la fonction read_yaml()
-parametres_init = parser.read_yaml()
+params = parser.read_yaml()
 # On imprime le contenu qui a été lu
-print("Données brutes\n:" + str(parametres_init) + "\n")
-print("Types des données lues:\n" + str(type(parametres_init)) + "\n")
-print(parametres_init["position"]["latitude"])
+print("Données brutes\n:" + str(params) + "\n")
+print("Types des données lues:\n" + str(type(params)) + "\n")
+print(params["latitude"])
 
 #-----------test de fonctions------------#
 
-performence = Performance()
+performence = Performance(params["finesse"],params["vitesse"],params["altitude"],params["dist_roulage_mini"],params["carburant_restant"],params["moteur_avion"])
 
 
-moteur_avion = False
-if moteur_avion:
+
+
+if params["moteur_avion"]:
     range_theorique = performence.range_moteur()
     print(f'range avec moteurs : {range_theorique} [nm]')
-    range_corrige = performence.correction_range()
+    range_corrige = performence.correction_range(range_theorique,Donnees.vents)
     print(f'range corrigé : {range_corrige} [nm]')
 else:
     range_theorique = performence.range_plane()
     print(f'range sans moteurs : {range_theorique} [nm]')
-    range_corrige = performence.correction_range()
+    range_corrige = performence.correction_range(range_theorique,Donnees.vents)
     print(f'range corrigé : {range_corrige} [nm]')
 
 
