23a8bdd97bc5beaab89543560f2979e987b7ee4e
SimGbd <133153776+SimGbd@users.noreply.github.com>
2023-06-13 15:14:29 -0400

    Update Donnees.py

--- Donnees.py
+++ Donnees.py
@@ -1,5 +1,9 @@
+import numpy as np 
+
 # --------------------------------------valeurs de test windy------------------------------------------
 
-vitesse_vent = 20  # homogène au reste de preference en [Kts]
-direction_vent = 57  # Direction du vent en degrés
+# test avec 8 points donc faire des liste de taille = nombre de cercle 
+nombre_point_cercle = 8
+vitesse_vent = np.random.uniform(0,100,nombre_point_cercle)  # homogène au reste de preference en [Kts]
+direction_vent = np.random.uniform(0,360,nombre_point_cercle)  # Direction du vent en degrés
 hauteur_aerodrome = 423
