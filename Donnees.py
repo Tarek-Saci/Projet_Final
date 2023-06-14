af1e44fc7bab5254f9bf011f6ca2e26b1355acfb
Tarek-Saci <tarek7saci@gmail.com>
2023-06-14 17:27:00 -0400

    Update Donnees.py

--- Donnees.py
+++ Donnees.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # test avec 8 points donc faire des liste de taille = nombre de cercle 
 nombre_points = 8
-vitesse_vent = np.random.uniform(0,100,nombre_point_cercle)  # homogène au reste de preference en [Kts]
-direction_vent = np.random.uniform(0,360,nombre_point_cercle)  # Direction du vent en degrés
+vitesse_vent = np.random.uniform(0,100,nombre_points)  # homogène au reste de preference en [Kts]
+direction_vent = np.random.uniform(0,360,nombre_points)  # Direction du vent en degrés
 
 vents = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16]])
 
