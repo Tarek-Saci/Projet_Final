18bd195afc10a53996c9e7e140d1e7cdc99277ad
Tarek-Saci <tarek7saci@gmail.com>
2023-06-14 15:20:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ parser = LecteurYAML('deck.yaml')
 # Lecture du fichier avec la fonction read_yaml()
 params = parser.read_yaml()
 # On imprime le contenu qui a été lu
-print("Données brutes\n:" + str(params) + "\n")
-print("Types des données lues:\n" + str(type(params)) + "\n")
-print(params["latitude"])
+#print("Données brutes\n:" + str(params) + "\n")
+#print("Types des données lues:\n" + str(type(params)) + "\n")
+#print(params["latitude"])
 
 #-----------test de fonctions------------#
 
-performence = Performance(params["finesse"],params["vitesse"],params["altitude"],params["dist_roulage_mini"],params["carburant_restant"],params["moteur_avion"])
+performence = Performance(params["finesse"],params["vitesse"],params["vitesse_plane"],params["altitude"],params["dist_roulage_mini"],params["carburant_restant"],params["moteur_avion"])
 
 
 
 
 if params["moteur_avion"]:
-    range_theorique = performence.range_moteur()
+    range_theorique = performence.range_moteur_theorique()
     print(f'range avec moteurs : {range_theorique} [nm]')
-    range_corrige = performence.correction_range(range_theorique,Donnees.vents)
+    range_corrige = performence.range_moteur_reel([-10,-10],[50,50])
     print(f'range corrigé : {range_corrige} [nm]')
 else:
     range_theorique = performence.range_plane()
     print(f'range sans moteurs : {range_theorique} [nm]')
     range_corrige = performence.correction_range(range_theorique,Donnees.vents)
     print(f'range corrigé : {range_corrige} [nm]')
-
 
 
 
