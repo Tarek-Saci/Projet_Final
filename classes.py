0d105253bc3f4ece8886da7058833e90c3fa572b
NatGitEts <nathan.carlier.1@ens.etsmtl.ca>
2023-06-14 17:00:02 -0400

    basemap_range

--- classes.py
+++ classes.py
@@ -1,13 +1,24 @@
+import yaml
+class LecteurYAML:
 
+    def __init__(self, file_path):
+        self.file_path = file_path
+    def read_yaml(self):
+        with open(self.file_path, 'r') as file:
+            try:
+                data = yaml.safe_load(file)
+                return data
+            except yaml.YAMLError as e:
+                print(f"Error reading YAML file: {e}")
 class Avion:
     def __init__(self,coordonnees_avion,range_avion):
         self.latitude = coordonnees_avion[0]
         self.longitude = coordonnees_avion[1]
-        self.range_avion = range_avion
+        self.range_theorique_avion = range_avion
 class Piste:
-    def __init__(self,longeur,surface):
+    def __init__(self,longeur):
         self.longeur = longeur
-        self.surface = surface
+        #self.surface = surface
 class Aerodrome:
     def __init__(self,objectid, nomcarto, codeindic, typeinfras, nbrpiste,longpiste2,
                     surface, latitude, longitude, acces):
@@ -20,27 +31,35 @@ class Aerodrome:
 
         longeur = []
         resultat_longeur = longpiste2.split("/")
-        if len(resultat_longeur) != nbrpiste:
+        if len(resultat_longeur) < nbrpiste:
             for i in range(0, int(nbrpiste)):
-                longeur.append(resultat_longeur)
+                longeur.append(resultat_longeur[0])
+        elif len(resultat_longeur) > nbrpiste:
+            for i in range(0, len(resultat_longeur)):
+                print(i)
+                longeur.append(resultat_longeur[i])
         else:
             for i in range(0, int(nbrpiste)):
                 longeur.append(resultat_longeur[i])
-        surface_liste = []
+        """surface_liste = []
         resultat_surface = surface.split(",")
         if len(resultat_surface) != nbrpiste:
             for i in range(0, int(nbrpiste)):
                 surface_liste.append(resultat_surface)
         else:
             for i in range(0, int(nbrpiste)):
-                surface_liste.append(resultat_surface[i])
-        for i in range(0,int(nbrpiste)):
-            self.pistes.append(Piste(longeur[i],surface_liste[i]))
+                surface_liste.append(resultat_surface[i])"""
+
+        if len(resultat_longeur) > nbrpiste:
+            for i in range(0, len(resultat_longeur)):
+                self.pistes.append(Piste(longeur[i]))
+        else:
+            for i in range(0,int(nbrpiste)):
+                self.pistes.append(Piste(longeur[i]))
 
         self.latitude = latitude
         self.longitude = longitude
         self.acces = acces
-
 def creer_aerodrome(objectid, nomcarto, codeindic, typeinfras, nbrpiste,longpiste2,
                     surface,latitude, longitude, acces):
     return Aerodrome(objectid, nomcarto, codeindic, typeinfras, nbrpiste,longpiste2,
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-
 import pandas as pd
 import numpy as np
 import fonctions as fc
 import classes
+
+# ---------- YAML ---------- #
+
+# Création de l'objet YAML qui lit le fichier "deck.yamL"
+parser = classes.LecteurYAML('deck.yaml')
+# Lecture du fichier avec la fonction read_yaml()
+parametres_init = parser.read_yaml()
+
+# ---------- INFORMATIONS AEROPORT.CSV ---------- #
 
 df = pd.read_csv("C:\\Users\\natha\\PycharmProjects\\test_cartopy\\Aeroport.csv")
 colonnes_souhaitees = ['objectid', 'nomcarto', 'codeindic', 'typeinfras', 'nbrpiste', 'longpiste2',
@@ -10,7 +18,6 @@ colonnes_souhaitees = ['objectid', 'nomcarto', 'codeindic', 'typeinfras', 'nbrpi
 df_colonnes = df[colonnes_souhaitees]
 aerodromes = []
 df_final = df_colonnes[(df_colonnes['typeinfras'] == 'Aéroport') | (df_colonnes['typeinfras'] == 'Aérodrome')].reset_index()
-
 objectid_array = df_final['objectid'].to_numpy()
 nomcarto_array = df_final['nomcarto'].to_numpy()
 codeindic_array = df_final['codeindic'].to_numpy()
@@ -22,25 +29,41 @@ latitude_array = df_final['latitude'].to_numpy()
 longitude_array = df_final['longitude'].to_numpy()
 acces_array = df_final['acces'].to_numpy()
 
+# ---------- CREATION DES AERODROMES ---------- #
+
 vecteur_creer_aerodrome = np.vectorize(classes.creer_aerodrome)
 
 aerodromes = vecteur_creer_aerodrome(objectid_array, nomcarto_array, codeindic_array,
                                      typeinfras_array, nbrpiste_array, longpiste2_array,
                                      surface_array, latitude_array, longitude_array, acces_array)
 
-coordonnees_avion = (49.603934,-75.402508)
+# ---------- CALCUL RANGE THEORIQUE ---------- #
 
-range_avion = 260
+range_theorique_avion = 400
 
-avion = classes.Avion(coordonnees_avion,range_avion)
+# ---------- CREATION DE L'AVION ---------- #
 
-coordonnes_sur_cercle,lons,lats = fc.cercle_range(range_avion,avion)
+coordonnees_avion = (parametres_init["latitude"],parametres_init["longitude"])
+avion = classes.Avion(coordonnees_avion,range_theorique_avion)
+
+coordonnes_sur_cercle,lons,lats = fc.cercle_range(range_theorique_avion,avion)
 
 list_coordonnes_sur_cercle = list(coordonnes_sur_cercle)
-distance = []
 
-for coordonnee in list_coordonnes_sur_cercle:
-    distance.append(fc.calcul_entre_deux_coordonnees(coordonnee, coordonnees_avion))
+#print(list_coordonnes_sur_cercle)
+
+# ---------- CREATION DES VENTS DES POINTS DU CERCLE ---------- #
+
+list_vents = []
+
+for point in list_coordonnes_sur_cercle:
+    list_vents.append(fc.calcul_vent(point[0],point[1],parametres_init["altitude"]))
+
+#print(f'Vents : {list_vents}')
+
+# ---------- CALCUL RANGE AVEC VENT ---------- #
+
+# ---------- CREATION DES AERODROMES DANS LA RANGE AVEC VENT ---------- #
 
 aerodromes_in_range = []
 
@@ -52,5 +75,38 @@ for aerodrome in aerodromes:
 lons_in_range = np.array([aerodrome.longitude for aerodrome in aerodromes_in_range])
 lats_in_range = np.array([aerodrome.latitude for aerodrome in aerodromes_in_range])
 
-fc.affichage_carte(aerodromes,avion,lons,lats,lons_in_range,lats_in_range)
+aerodromes_in_range_right_size = []
 
+for aerodrome in aerodromes_in_range:
+    resultat,numero_pistes = fc.cherche_longueur_piste(aerodrome,parametres_init["dist_roulage_mini"])
+    if resultat == True:
+        aerodromes_in_range_right_size.append(aerodrome)
+
+lons_in_range_in_size = np.array([aerodrome.longitude for aerodrome in aerodromes_in_range_right_size])
+lats_in_range_in_size = np.array([aerodrome.latitude for aerodrome in aerodromes_in_range_right_size])
+
+# ---------- AERODROME LE PLUS PROCHE ---------- #
+
+distance = np.array(fc.calcul_entre_deux_coordonnees(coordonnees_avion,lats_in_range_in_size,lons_in_range_in_size))
+print(distance)
+print(len(aerodromes_in_range_right_size))
+
+min_value = np.min(distance)
+min_index = np.argmin(distance)
+
+print("Valeur minimale :", min_value)
+print("Indice correspondant :", min_index)
+
+lat_aerodrome_plus_proche = aerodromes_in_range_right_size[min_index].latitude
+
+lon_aerodrome_plus_proche = aerodromes_in_range_right_size[min_index].longitude
+
+# ---------- CAP AEROPORT LE PLUS PROCHE ---------- #
+
+new_cap = fc.calcul_new_cap(avion.latitude,avion.longitude,lat_aerodrome_plus_proche,lon_aerodrome_plus_proche)
+
+print(new_cap)
+
+# ---------- AFFICHAGE DE LA CARTE FINALE ---------- #
+
+fc.affichage_carte(aerodromes,avion,lons,lats,lons_in_range,lats_in_range,lons_in_range_in_size,lats_in_range_in_size)
