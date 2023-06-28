4f53d68aeb5f7008f046567cb566683d6785e4fc
SimGbd <133153776+SimGbd@users.noreply.github.com>
2023-06-28 17:45:38 -0400

    Changement pour integrer temperature

--- fonctions.py
+++ fonctions.py
@@ -37,10 +37,11 @@ def flatten_json(data, prefix=''):
 # L'argument doit être entré en mètres
 def calcul_altitude_pression(altitude):
     #d'après l'ISA
-    if altitude<11000:
-        pression = 1013.25*(1-0.0065*altitude/288.15)**(9.81/(0.0065*287.04))
+    altitude_m=altitude/3.28
+    if altitude_m<11000:
+        pression = 1013.25*(1-0.0065*altitude_m/288.15)**(9.81/(0.0065*287.04))
     else:
-        pression = 226.32*math.exp(-9.81*(altitude-11000)/(287.04*216.65))
+        pression = 226.32*math.exp(-9.81*(altitude_m-11000)/(287.04*216.65))
     return pression
 
 # Fonction permettant de déterminer le vecteur vitesse du vent à une position et une altitude donnée
