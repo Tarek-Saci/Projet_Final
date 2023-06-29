7b098b401ebc3030b7e867e7a8e9ecd93e8731fd
SimGbd <133153776+SimGbd@users.noreply.github.com>
2023-06-29 17:17:46 -0400

    Update range_avec_virage.py

--- range_avec_virage.py
+++ range_avec_virage.py
@@ -22,7 +22,7 @@ def distance_avec_virage(vitesse, x_avion, y_avion, x_aero, y_aero, cap):
     print(vect_av_aero)
 
     #Calcul de l'angle entre le cap et l'aéroport
-    angle_cap_aero = new_cap - math.degrees(math.acos(vect_av_aero[0]/(vect_av_aero[0]**2+vect_av_aero[1]**2)**0.5))
+    angle_cap_aero = angle_cap - math.degrees(math.acos(vect_av_aero[0]/(vect_av_aero[0]**2+vect_av_aero[1]**2)**0.5))
     print('angle cap aero', angle_cap_aero)
 
     #Calcul du rayon minimal de virage (le facteur de charge maximale pour un avion civil est généralement n=1.19)
