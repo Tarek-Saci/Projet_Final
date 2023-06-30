6997a636ea37f21a52615ab294c2f597116c8ee5
bathom24 <133153824+bathom24@users.noreply.github.com>
2023-06-30 17:32:02 +0200

    ajout commentaires et modif affichage

--- SolutionAtterrissage/atmosphere/vent_windy.py
+++ SolutionAtterrissage/atmosphere/vent_windy.py
@@ -1,7 +1,14 @@
-#Ce programme permet d'obtenir en temps réel les données météorologiques en un point donné
-#Ce point doit être repéré par ses coordonnées GPS (latitude, longitude). La latitude doit être un flottant
-# compris entre -89.99° et 90° tandis que la longitude peut être un flottant quelconque
 def get_windy_data(lat, lon):
+    """
+    Cette fonction permet d'obtenir en temps réel les données météorologiques en un point donné
+
+    Args:
+        lat (float): La latitude du point recherché compris entre -89.99° et 90°
+        lon (float): La longitude du point recherché appartenant aux nombres réels
+
+    Returns:
+        data: base de données contenant les données sous forme json
+    """
     url_base = "https://node.windy.com/forecast/meteogram/ecmwf/"
     url_request = url_base + str(lat) + "/" + str(lon)
     import urllib.request, json
