7317f3728750e26caf1e17eb8bc32ce041735b84
Tarek-Saci <tarek7saci@gmail.com>
2023-06-30 23:52:56 -0400

    Delete __init__.py

--- SolutionAtterrissage/aviation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Donnees import * # a revoir si on le garde on si on introduit les données utiles aux endroits nécessaires
-from .fonction_range import *
-from .fonctions import * # a revoir ce fichier
-
